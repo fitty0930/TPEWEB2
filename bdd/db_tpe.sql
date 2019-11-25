8f41f2829925ce740c684c0ca549cfa95fe463d4
martin <martinaguirre08@gmail.com>
2019-11-25 20:15:25 -0300

    agrega db

--- bdd/db_tpe.sql
+++ bdd/db_tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2019 a las 21:27:45
+-- Tiempo de generación: 26-11-2019 a las 00:13:03
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.3.9
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_tpe`
 --
-CREATE DATABASE IF NOT EXISTS `db_tpe` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db_tpe`;
 
 -- --------------------------------------------------------
 
@@ -30,11 +28,10 @@ USE `db_tpe`;
 -- Estructura de tabla para la tabla `categorias`
 --
 
-CREATE TABLE IF NOT EXISTS `categorias` (
-  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `categorias`
@@ -48,13 +45,34 @@ INSERT INTO `categorias` (`id_categoria`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `texto` varchar(500) NOT NULL,
+  `puntaje` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id_comentario`, `texto`, `puntaje`, `id_producto`, `id_usuario`) VALUES
+(12, 'hola', 1, 7, 1),
+(19, 'El producto me parecio fasdoifaojsdf', 4, 7, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `gcaptcha`
 --
 
-CREATE TABLE IF NOT EXISTS `gcaptcha` (
+CREATE TABLE `gcaptcha` (
   `web_key` varchar(200) NOT NULL,
-  `secret_key` varchar(200) NOT NULL,
-  PRIMARY KEY (`web_key`)
+  `secret_key` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -67,18 +85,37 @@ INSERT INTO `gcaptcha` (`web_key`, `secret_key`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id_imagen` int(11) NOT NULL,
+  `ruta` varchar(255) DEFAULT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id_imagen`, `ruta`, `id_producto`) VALUES
+(182, 'imagenes/5ddc54846163c.jpg', 14),
+(185, 'imagenes/5ddc590b19b32.jpg', 34),
+(186, 'imagenes/5ddc5f0408ffa.jpg', 14);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE IF NOT EXISTS `productos` (
-  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
   `id_categoria` int(11) NOT NULL,
   `producto` varchar(20) NOT NULL,
   `marca` varchar(20) NOT NULL,
-  `precio` int(11) NOT NULL,
-  PRIMARY KEY (`id_producto`),
-  KEY `id_categoría` (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+  `precio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `productos`
@@ -87,8 +124,8 @@ CREATE TABLE IF NOT EXISTS `productos` (
 INSERT INTO `productos` (`id_producto`, `id_categoria`, `producto`, `marca`, `precio`) VALUES
 (7, 1, 'Auriculares', 'Genius', 200),
 (13, 3, 'Playstation', 'Sony', 1000),
-(14, 2, 'S3', 'Motorola', 200),
-(16, 3, 'Playstation', 'Sony', 1000);
+(14, 2, 'S5', 'Motorola', 200),
+(34, 3, 'play 2', 'sony', 3000);
 
 -- --------------------------------------------------------
 
@@ -96,24 +133,115 @@ INSERT INTO `productos` (`id_producto`, `id_categoria`, `producto`, `marca`, `pr
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
   `nombre_usuario` varchar(15) NOT NULL,
   `password` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `password`) VALUES
-(1, 'martin0930', '$2y$10$lalWJ3mJqRWRmGTbPMtsU.XGmJ8//mGUTpH21U4jjw5vVjHnauV.C'),
-(2, 'usuario123', '$2y$10$LSfX6H8tdAA/rUJ5mDIO.e7MV8NlTAyaEr83yKXdHKYZ9actu4VAS');
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `password`, `admin`) VALUES
+(1, 'martin0930', '$2y$10$lalWJ3mJqRWRmGTbPMtsU.XGmJ8//mGUTpH21U4jjw5vVjHnauV.C', 1),
+(14, '12345', '$2y$10$YOoNHylNJsgfK9ZIakmQYeZk2.QdTb/kFHeGeboTKDL4n4R.gAAN2', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `gcaptcha`
+--
+ALTER TABLE `gcaptcha`
+  ADD PRIMARY KEY (`web_key`);
+
+--
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id_imagen`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_categoría` (`id_categoria`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=187;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE,
+  ADD CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `imagen_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `productos`
